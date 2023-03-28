19745cbc8f33d016bda084c89e8ee2c1621f0ef7
szopszop <szymontracz1@gmail.com>
2023-03-28 12:34:16 +0200

    inner class implementation

--- src/main/java/design/patterns/builder/Main.java
+++ src/main/java/design/patterns/builder/Main.java
@@ -5,6 +5,12 @@ import design.patterns.builder.house.House;
 public class Main {
 
     public static void main(String[] args) {
-        House house = new House();
+        House house = new House.HouseBuilder()
+                .buildWalls("walls")
+                .buildFloors("floors")
+                .buildRooms("rooms")
+                .build();
+        System.out.println(house);
     }
+
 }
