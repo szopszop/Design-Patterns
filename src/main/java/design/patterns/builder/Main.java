b3b3c54415208e3769496f66bb098329945bef55
szopszop <szymontracz1@gmail.com>
2023-03-28 12:17:47 +0200

    created initial problem to solve

---
+++ src/main/java/design/patterns/builder/Main.java
@@ -0,0 +1,10 @@
+package design.patterns.builder;
+
+import design.patterns.builder.house.House;
+
+public class Main {
+
+    public static void main(String[] args) {
+        House house = new House();
+    }
+}
---
+++ src/main/java/design/patterns/builder/house/House.java
@@ -0,0 +1,17 @@
+package design.patterns.builder.house;
+
+import lombok.Data;
+
+@Data
+public class House {
+
+    private String walls;
+    private String floors;
+    private String rooms;
+    private String roof;
+    private String windows;
+    private String doors;
+    private String garage;
+
+
+}
