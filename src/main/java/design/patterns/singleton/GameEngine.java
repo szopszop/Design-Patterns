aff8278926e30cc9aa7a48d6c8f78dc9936c46d1
szopszop <szymontracz1@gmail.com>
2023-03-28 11:31:47 +0200

    initiate env for singleton

---
+++ src/main/java/design/patterns/singleton/GameEngine.java
@@ -0,0 +1,14 @@
+package design.patterns.singleton;
+
+public class GameEngine {
+
+    private int hp = 100;
+    private String characterName = "Hero";
+
+    public void run() {
+        while (true) {
+            // wait for input
+            // change game state
+        }
+    }
+}
